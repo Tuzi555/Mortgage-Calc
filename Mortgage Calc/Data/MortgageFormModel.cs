3eb8cf33d96c65f9f80abd5278643b122ee14353
Tuzi555 <jakub.tuzar@seznam.cz>
2022-06-28 16:22:11 +0200

    Pulled out calculation logic from page to Mortgage class and added calculation for total interest and total paid amount.

---
+++ Mortgage Calc/Data/MortgageFormModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Mortgage_Calc.Data;
+
+public class Mortgage
+{
+    [Required] public int? LoanAmount { get; set; }
+    [Required] public double? Rate { get; set; }
+    [Required] public int? Term { get; set; }
+
+    public double MonthlyPayment { get; set; }
+    public double TotalPaid { get; set; }
+    public double TotalInterest { get; set; }
+    public void CalculateMortgage()
+    {
+        var monthlyRate = ((Rate ?? 0) / 100) / 12;
+        var months = (Term ?? 0) * 12;
+        var loan = LoanAmount ?? 0;
+        MonthlyPayment = loan * ((monthlyRate * Math.Pow((1 + monthlyRate), months)) / (Math.Pow((1 + monthlyRate), months) - 1));
+        TotalPaid = months * MonthlyPayment;
+        TotalInterest = TotalPaid - loan;
+    }
+}
