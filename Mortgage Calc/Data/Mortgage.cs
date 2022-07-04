56909f13cc02bd85c7e01185eb757b056690014a
Tuzi555 <jakub.tuzar@seznam.cz>
2022-07-04 14:07:00 +0200

    Prepared draft from template in calculator.html

--- Mortgage Calc/Data/Mortgage.cs
+++ Mortgage Calc/Data/Mortgage.cs
@@ -4,14 +4,14 @@ namespace Mortgage_Calc.Data;
 
 public class Mortgage
 {
-    [Required(ErrorMessage = "Loan amount is required.")] 
+    [Required(ErrorMessage = "Loan amount is required.")]
     [Range(1, Int64.MaxValue, ErrorMessage = "Loan amount must be positive whole number.")]
     public int? LoanAmount { get; set; }
-    [Required(ErrorMessage = "Interest rate is required.")] 
-    [Range(0,100, ErrorMessage = "Interest must be decimal number between 0 and 100.")]
+    [Required(ErrorMessage = "Interest rate is required.")]
+    [Range(0, 100, ErrorMessage = "Interest must be decimal number between 0 and 100.")]
     public double? Rate { get; set; }
-    [Required(ErrorMessage = "Term is required.")] 
-    [Range(1,Int64.MaxValue, ErrorMessage = "Term must be positive whole number larger than 0.")]
+    [Required(ErrorMessage = "Term is required.")]
+    [Range(1, Int64.MaxValue, ErrorMessage = "Term must be positive whole number larger than 0.")]
     public int? Term { get; set; }
 
     public double MonthlyPayment { get; set; }
